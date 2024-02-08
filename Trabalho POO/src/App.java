fcb0a62326958b552b2be159692b134f16068118
Breno Oliveira Cavalcante <breno.cavalcante@ufu.br>
2024-02-08 09:08:05 -0300

    inicio

---
+++ Trabalho POO/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        cliente leleu = new cliente();
+        leleu.nome = 1;
+        System.err.println(leleu);
+    }
+}
