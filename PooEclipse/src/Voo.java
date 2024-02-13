8ce65a73e32af612c83251b7c657f0d4fee2a326
Breno Oliveira Cavalcante <breno.cavalcante@ufu.br>
2024-02-13 17:28:09 -0300

    Classe voo

---
+++ PooEclipse/src/Voo.java
@@ -0,0 +1,11 @@
+
+public class Voo {
+	private String data;
+	private String hora_programada_chegada;
+	private String hora_programada_partida;
+	private int nro_vagas_disponiveis;
+	private Trecho_de_Voo trecho;
+	private float preco_da_passagem;
+	
+	
+}
--- PooEclipse/src/principal.java
+++ PooEclipse/src/principal.java
@@ -3,7 +3,7 @@ public class principal {
 
 	public static void main(String[] args) {
 		//jamalzin
-		
+		System.out.printf("Éerre");
 	}
 
 }
