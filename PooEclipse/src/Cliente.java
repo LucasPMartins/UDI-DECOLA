682dd3409dbaea3d482717f51b7911d7d8d1708f
Lucas Araújo de Oliveira <lucas.oliveira7@ufu.br>
2024-02-13 16:45:09 -0300

    ff

---
+++ PooEclipse/src/Cliente.java
@@ -0,0 +1,11 @@
+
+public class Cliente extends Pessoa{
+	
+	
+
+	public Cliente(String cpf, String endereco, String nome, String data_nasc) {
+		super(cpf,endereco, nome, data_nasc);
+		
+	}
+	
+}
--- PooEclipse/src/Pessoa.java
+++ PooEclipse/src/Pessoa.java
@@ -3,5 +3,18 @@ public class Pessoa {
 	
 	private String cpf;
 	private String endereco;
+	private String nome;
+	private String data_nasc;
+	
+	
+	public Pessoa(String cpf, String endereco, String nome, String data_nasc) {
+		super();
+		this.cpf = cpf;
+		this.endereco = endereco;
+		this.nome = nome;
+		this.data_nasc = data_nasc;
+	}
+	
+	
 	
 }
