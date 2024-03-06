90d1fe7ccba0cada5296ee811c7ad50b840c06d0
Lucas Araújo de Oliveira <lucas.oliveira7@ufu.br>
2024-03-06 20:35:24 -0300

    c10

--- PooEclipse/src/ClienteVip.java
+++ PooEclipse/src/ClienteVip.java
@@ -1,5 +1,7 @@
 
 public class ClienteVip extends Cliente {
+	
+	private static int nro_limite; 
     private float desconto;
 
     public ClienteVip(String cpf, String nome, float desconto) {
@@ -10,4 +12,18 @@ public class ClienteVip extends Cliente {
     public float getDesconto() {
         return desconto;
     }
+
+	public static int getNro_limite() {
+		return nro_limite;
+	}
+
+	public static void setNro_limite(int nro_limite) {
+		ClienteVip.nro_limite = nro_limite;
+	}
+
+	public void setDesconto(float desconto) {
+		this.desconto = desconto;
+	}
+    
+
 }
