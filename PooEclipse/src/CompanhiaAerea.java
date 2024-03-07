ce4790e4c0c005fe45bbdd3da96f0fba3690b387
Breno Oliveira Cavalcante <breno.cavalcante@ufu.br>
2024-03-07 15:40:01 -0300

    companhia aerea taxa

--- PooEclipse/src/CompanhiaAerea.java
+++ PooEclipse/src/CompanhiaAerea.java
@@ -3,5 +3,6 @@ public class CompanhiaAerea extends Empresa{
 	public CompanhiaAerea(String cnpj, String nome_oficial, String nome_divulgacao, Data data_criacao) {
 		super(cnpj,nome_oficial,nome_divulgacao,data_criacao);
 	}
+	private static int taxa_da_udi_decola = 1000;
 	
 }
