972b92a722c5870b61b9702718c4aac5a7d6e38b
Lucas Martins <127567616+LucasPMartins@users.noreply.github.com>
2024-03-07 15:44:29 -0300

    mm

--- PooEclipse/src/Pessoa.java
+++ PooEclipse/src/Pessoa.java
@@ -14,31 +14,25 @@ public abstract class  Pessoa {
 		this.data_nasc = data_nasc;
 	}
 
-
 	public String getCpf() {
 		return cpf;
 	}
-
 
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
 
-
 	public String getEndereco() {
 		return endereco;
 	}
-
 
 	public void setEndereco(String endereco) {
 		this.endereco = endereco;
 	}
 
-
 	public String getNome() {
 		return nome;
 	}
-
 
 	public void setNome(String nome) {
 		this.nome = nome;
