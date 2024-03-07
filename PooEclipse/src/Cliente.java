ad7721b7705183ef5879203b5cdb9504820f93ec
Lucas Martins <127567616+LucasPMartins@users.noreply.github.com>
2024-03-07 15:31:22 -0300

    compra hotel

--- PooEclipse/src/Cliente.java
+++ PooEclipse/src/Cliente.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Cliente extends Pessoa{
 	
@@ -8,6 +9,9 @@ public class Cliente extends Pessoa{
 	private boolean vip;
 	private static int nro_limite;
 	
+	public int getQtdCompras() {
+		return historico.size();
+	}
 	
 	public Cliente(String cpf, String nome,String email, Data data_cadastro, String endereco,Data nascimento, Data virou_VIP) {
 		super(cpf,endereco, nome,nascimento);
@@ -19,30 +23,32 @@ public class Cliente extends Pessoa{
 	public static int getNro_limite() {
 		return nro_limite;
 	}
+	
+	public boolean isVip() {
+		return vip;
+	}
+
+	public void setVip(boolean vip) {
+		this.vip = vip;
+	}
 
 	public static void setNro_limite(int nro_limite) {
-		ClienteVip.nro_limite = nro_limite;
+		Cliente.nro_limite = nro_limite;
 	}
-	
+
 	public void addCompra(Compra compra) {
 		this.historico.add(compra);
-		if(historico.size() >= nro_limite) {
-			setVip(True);
+		if(getQtdCompras() >= nro_limite) {
+			setVip(true);
 		}
 	}
 
-	
-	public Cliente(String cpf, String nome) {
-		super(cpf,"", nome,"");
-		email = "";
-		data_cadastro = "";
+	public Cliente(String cpf, String endereco, String nome, Data data_nasc, String email, Data data_cadastro) {
+		super(cpf, endereco, nome, data_nasc);
+		this.email = email;
+		this.data_cadastro = data_cadastro;
+		this.setVip(false);
 	}
-	
-	
-    public static int getCertaQtdCompras() {
-        return certa_qtd_compras;
-    }
-	
 
 	public String getEmail() {
 		return email;
@@ -60,24 +66,6 @@ public class Cliente extends Pessoa{
 		this.data_cadastro = data_cadastro;
 	}
 
-	public Pesquisa_Do_Cliente getRegistro() {
-		return registro;
-	}
-
-	public void setRegistro(Pesquisa_Do_Cliente registro) {
-		this.registro = registro;
-	}
-
-
-	public static int getCerta_qtd_compras() {
-		return certa_qtd_compras;
-	}
-
-
-	public static void setCerta_qtd_compras(int certa_qtd_compras) {
-		Cliente.certa_qtd_compras = certa_qtd_compras;
-	}
-	
 
 
 	
