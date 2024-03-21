f536d3879834e489c860ff97a2c65d9806f01e86
Breno Oliveira Cavalcante <breno.cavalcante@ufu.br>
2024-03-21 20:05:17 -0300

    interface

---
+++ PooEclipse/src/InterfacePrincipal.java
@@ -0,0 +1,43 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class InterfacePrincipal {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					InterfacePrincipal window = new InterfacePrincipal();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public InterfacePrincipal() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	
+
+}
