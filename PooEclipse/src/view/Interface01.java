fe7b8aca826844ae773a04e716e34dd78f7007dd
Lucas Martins <127567616+LucasPMartins@users.noreply.github.com>
2024-03-29 20:15:32 -0300

    novas interfaces

--- PooEclipse/src/view/Interface01.java
+++ PooEclipse/src/view/Interface01.java
@@ -1,144 +1,111 @@
 package view;
-import java.awt.Color;
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextArea;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.*;
-import java.awt.*;
 
 public class Interface01 extends JFrame {
 
-    private static final long serialVersionUID = 1L;
-    private JPanel corpo;
-    private JTextField campoUsuario;
-    private JPasswordField campoSenha;
-    private JButton logar; 
-    private JPanel principal;
-    private JButton noturno_modo;
-    private JLabel usuario_label;
-    private JLabel senha_label;
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
 
-    /**
-     * Launch the application.
-     */
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    Interface01 frame = new Interface01();
-                    frame.setLocationRelativeTo(null); //A tela inicia no meio.
-                    frame.setResizable(isDefaultLookAndFeelDecorated());
-                    frame.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Interface01 frame = new Interface01();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
-    /**
-     * Create the frame.
-     */
-    public Interface01() {
-        setTitle("Login");
-        setFont(new Font("JetBrains Mono", Font.PLAIN, 14));
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 450, 300);
-        corpo = new JPanel();
-        corpo.setBorder(new EmptyBorder(5, 5, 5, 5));
-        setContentPane(corpo);
-        corpo.setLayout(null);
-        Color[] colors = {Color.decode("#1A1A1A"), Color.decode("#F3F3F3")}; // Cores padrao
-        corpo.setBackground(colors[1]); //COR PADRAO DE FUNDO
-        
-        
-        noturno_modo = new JButton("Modo Noturno");
-        noturno_modo.setBounds(129, 16, 165, 19);
-        noturno_modo.setFont(new Font("Arial", Font.PLAIN, 18));
-        noturno_modo.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-            	// BOTAO DE Modo noturno
-                Color cor1 = (corpo.getBackground().equals(colors[0])) ? colors[1] : colors[0];
-                Color cor2 = colors[0];
-                if(cor1.equals(colors[0])) {
-                    cor2 = colors[1];
-                }
-                corpo.setBackground(cor1);
-                campoUsuario.setBackground(cor2);
-                campoSenha.setBackground(cor2);
-                noturno_modo.setBackground(cor2);
-                logar.setBackground(cor2);
-                principal.setBackground(cor1);
-                
-                corpo.setForeground(cor2);
-                campoUsuario.setForeground(cor1);
-                campoSenha.setForeground(cor1);
-                noturno_modo.setForeground(cor1);
-                logar.setForeground(cor1);
-                usuario_label.setForeground(cor2);
-                senha_label.setForeground(cor2);
-                
-                
-            }
-        });
-        corpo.add(noturno_modo);
-        
-        principal = new JPanel();
-        principal.setBounds(58, 38, 310, 207);
-        corpo.add(principal);
-        principal.setBackground(colors[0]); //DEFININDO COR PADRAO
-        principal.setLayout(null);
-        
-        campoUsuario = new JTextField();
-        campoUsuario.setToolTipText("");
-        campoUsuario.setForeground(Color.WHITE);
-        campoUsuario.setBounds(25, 36, 258, 26);
-        principal.add(campoUsuario);
-        campoUsuario.setColumns(10);
-        campoUsuario.setBackground(colors[0]); // DEFININDO COR PADRAO
-       
-        campoSenha = new JPasswordField();
-        campoSenha.setForeground(Color.WHITE);
-        campoSenha.setBounds(25, 89, 258, 26);
-        principal.add(campoSenha);
-        campoSenha.setBackground(colors[0]); // DEFININDO COR PADRAO
-        
-        logar = new JButton("Login");
-        logar.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        		//QUANDO O BOTAO É CLICADO:
-        		if(campoUsuario.getText()!=null && !campoUsuario.getText().isEmpty() &&
-        				campoSenha.getText()!=null && !campoSenha.getText().isEmpty()) {
-        			//tem coisa valida dentro da caixa 
-        			JOptionPane.showMessageDialog(logar, "Informaçoes corretas", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
-        			
-        			 InterfaceHotel inter = new InterfaceHotel();
-        			 inter.setLocationRelativeTo(null);
-        			 inter.setVisible(true); // Correção aqui
-        			 dispose();
-        		}
-        		else {
-        			JOptionPane.showMessageDialog(logar, "Informaçoes incorretas", "Aviso", JOptionPane.WARNING_MESSAGE);
-        		}
-        	}
-        });
-        logar.setBounds(97, 150, 115, 29);
-        principal.add(logar);
-        logar.setFont(new Font("JetBrains Mono", Font.PLAIN, 16));
-        logar.setBackground(colors[0]); // DEFININDO COR PADRAO
-        
-        usuario_label = new JLabel("Usuário");
-        usuario_label.setBounds(25, 16, 115, 20);
-        principal.add(usuario_label);
-        
-        senha_label = new JLabel("Senha");
-        senha_label.setBounds(25, 66, 69, 20);
-        principal.add(senha_label);
-    }
+	/**
+	 * Create the frame.
+	 */
+	public Interface01() {
+		setTitle("UDI-Decola");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 584, 394);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Bem Vindo ao UDI-Decola");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNewLabel.setBounds(20, 21, 254, 20);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("O que é?");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_1.setBounds(30, 69, 72, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel(".");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNewLabel_2.setBounds(20, 65, 14, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		JTextArea txtrOUdidecola = new JTextArea();
+		txtrOUdidecola.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		txtrOUdidecola.setEditable(false);
+		txtrOUdidecola.setText("O Udi-Decola é uma companhia que permite realizar buscas e compras de passagens \r\naéreas e diárias em hotéis. ");
+		txtrOUdidecola.setBounds(20, 94, 526, 36);
+		contentPane.add(txtrOUdidecola);
+		
+		JLabel lblNewLabel_2_1 = new JLabel(".");
+		lblNewLabel_2_1.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNewLabel_2_1.setBounds(20, 147, 14, 14);
+		contentPane.add(lblNewLabel_2_1);
+		
+		JLabel lblNewLabel_3 = new JLabel("Como podemos te ajudar?");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_3.setBounds(30, 151, 164, 14);
+		contentPane.add(lblNewLabel_3);
+		
+		JButton btnNewButton = new JButton("Cadastrar");
+		btnNewButton.setBounds(52, 210, 105, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Login");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_1.setBounds(52, 176, 105, 23);
+		contentPane.add(btnNewButton_1);
+		
+		JLabel lblNewLabel_2_1_1 = new JLabel(".");
+		lblNewLabel_2_1_1.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNewLabel_2_1_1.setBounds(20, 244, 14, 14);
+		contentPane.add(lblNewLabel_2_1_1);
+		
+		JLabel lblNewLabel_4 = new JLabel("Deseja ser parceiro do UDI-Decola?");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_4.setBounds(30, 248, 244, 14);
+		contentPane.add(lblNewLabel_4);
+		
+		JButton btnNewButton_1_1 = new JButton("Login");
+		btnNewButton_1_1.setBounds(52, 273, 105, 23);
+		contentPane.add(btnNewButton_1_1);
+		
+		JButton btnNewButton_2 = new JButton("Cadastrar");
+		btnNewButton_2.setBounds(52, 307, 105, 23);
+		contentPane.add(btnNewButton_2);
+	}
 }
---
+++ PooEclipse/src/view/InterfaceCadastro02.java
@@ -0,0 +1,186 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JComboBox;
+
+public class InterfaceCadastro02 extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
+	private JTextField textField_8;
+	private JTextField textField_9;
+	private JTextField textField_10;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					InterfaceCadastro02 frame = new InterfaceCadastro02();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public InterfaceCadastro02() {
+		setResizable(false);
+		setTitle("Cadastro de Funcionario Parceiro");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 589, 382);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Informe seus Dados:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel.setBounds(10, 10, 209, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("CPF:");
+		lblNewLabel_1.setBounds(20, 36, 46, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		textField.setBounds(53, 33, 193, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Endereço:");
+		lblNewLabel_2.setBounds(20, 64, 62, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(81, 61, 325, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("Nome:");
+		lblNewLabel_3.setBounds(20, 93, 46, 14);
+		contentPane.add(lblNewLabel_3);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(68, 89, 333, 20);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		JLabel lblNewLabel_12 = new JLabel("Data de Nascimento:");
+		lblNewLabel_12.setBounds(299, 33, 137, 14);
+		contentPane.add(lblNewLabel_12);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(422, 30, 33, 20);
+		contentPane.add(textField_3);
+		
+		JLabel lblNewLabel_13 = new JLabel("/");
+		lblNewLabel_13.setBounds(459, 33, 21, 14);
+		contentPane.add(lblNewLabel_13);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(465, 30, 33, 20);
+		contentPane.add(textField_4);
+		
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		textField_5.setBounds(508, 30, 33, 20);
+		contentPane.add(textField_5);
+		
+		JLabel lblNewLabel_14 = new JLabel("/");
+		lblNewLabel_14.setBounds(502, 33, 21, 14);
+		contentPane.add(lblNewLabel_14);
+		
+		JLabel lblNewLabel_4 = new JLabel("E-mail:");
+		lblNewLabel_4.setBounds(20, 121, 46, 14);
+		contentPane.add(lblNewLabel_4);
+		
+		textField_6 = new JTextField();
+		textField_6.setBounds(67, 118, 303, 20);
+		contentPane.add(textField_6);
+		textField_6.setColumns(10);
+		
+		JLabel lblNewLabel_5 = new JLabel("Usuario:");
+		lblNewLabel_5.setBounds(20, 220, 62, 14);
+		contentPane.add(lblNewLabel_5);
+		
+		textField_7 = new JTextField();
+		textField_7.setBounds(69, 217, 150, 20);
+		contentPane.add(textField_7);
+		textField_7.setColumns(10);
+		
+		JLabel lblNewLabel_6 = new JLabel("Senha:");
+		lblNewLabel_6.setBounds(20, 255, 62, 14);
+		contentPane.add(lblNewLabel_6);
+		
+		textField_8 = new JTextField();
+		textField_8.setBounds(69, 252, 150, 20);
+		contentPane.add(textField_8);
+		textField_8.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Confirmar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(459, 309, 104, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Voltar");
+		btnNewButton_1.setBounds(332, 309, 104, 23);
+		contentPane.add(btnNewButton_1);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(132, 180, 137, 20);
+		contentPane.add(comboBox);
+		
+		JLabel lblNewLabel_7 = new JLabel("Carteira de Trabalho:");
+		lblNewLabel_7.setBounds(20, 152, 130, 14);
+		contentPane.add(lblNewLabel_7);
+		
+		textField_9 = new JTextField();
+		textField_9.setBounds(150, 149, 185, 20);
+		contentPane.add(textField_9);
+		textField_9.setColumns(10);
+		
+		JLabel lblNewLabel_8 = new JLabel("Salário:");
+		lblNewLabel_8.setBounds(345, 152, 56, 14);
+		contentPane.add(lblNewLabel_8);
+		
+		textField_10 = new JTextField();
+		textField_10.setBounds(397, 149, 86, 20);
+		contentPane.add(textField_10);
+		textField_10.setColumns(10);
+		
+		JLabel lblNewLabel_9 = new JLabel("Local de Trabalho:");
+		lblNewLabel_9.setBounds(20, 183, 115, 14);
+		contentPane.add(lblNewLabel_9);
+	}
+}
