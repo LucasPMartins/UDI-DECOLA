0f2ad1140190b628f3d6f115e95b771b39d095a5
Breno Oliveira Cavalcante <breno.cavalcante@ufu.br>
2024-04-10 17:16:24 -0300

    InterfaceCadastroCliente
    adicionei algumas coisas

--- PooEclipse/src/view/InterfaceCadastroCliente.java
+++ PooEclipse/src/view/InterfaceCadastroCliente.java
@@ -1,4 +1,6 @@
 package view;
+import classesPrincipais.*;
+import dados.*;
 
 import java.awt.EventQueue;
 
@@ -9,20 +11,24 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class InterfaceCadastroCliente extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
+	private JTextField CPFlabel;
+	private JTextField EnderecoLabel;
+	private JTextField NomeLabel;
 	private JTextField textField_3;
 	private JTextField textField_4;
 	private JTextField textField_5;
-	private JTextField textField_6;
-	private JTextField textField_7;
-	private JTextField textField_8;
+	private JTextField EmailLabel;
+	private JTextField usuarioLabel;
+	private JTextField SenhaLabel;
+	
+	private DadosCliente clientes;
 
 	/**
 	 * Launch the application.
@@ -63,28 +69,28 @@ public class InterfaceCadastroCliente extends JFrame {
 		lblNewLabel_1.setBounds(20, 36, 46, 14);
 		contentPane.add(lblNewLabel_1);
 		
-		textField = new JTextField();
-		textField.setBounds(53, 33, 193, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		CPFlabel = new JTextField();
+		CPFlabel.setBounds(53, 33, 193, 20);
+		contentPane.add(CPFlabel);
+		CPFlabel.setColumns(10);
 		
 		JLabel lblNewLabel_2 = new JLabel("Endereço:");
 		lblNewLabel_2.setBounds(20, 64, 62, 14);
 		contentPane.add(lblNewLabel_2);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(84, 61, 325, 20);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
+		EnderecoLabel = new JTextField();
+		EnderecoLabel.setBounds(84, 61, 325, 20);
+		contentPane.add(EnderecoLabel);
+		EnderecoLabel.setColumns(10);
 		
 		JLabel lblNewLabel_3 = new JLabel("Nome:");
 		lblNewLabel_3.setBounds(20, 93, 46, 14);
 		contentPane.add(lblNewLabel_3);
 		
-		textField_2 = new JTextField();
-		textField_2.setBounds(76, 90, 333, 20);
-		contentPane.add(textField_2);
-		textField_2.setColumns(10);
+		NomeLabel = new JTextField();
+		NomeLabel.setBounds(76, 90, 333, 20);
+		contentPane.add(NomeLabel);
+		NomeLabel.setColumns(10);
 		
 		JLabel lblNewLabel_12 = new JLabel("Data de Nascimento:");
 		lblNewLabel_12.setBounds(299, 33, 137, 14);
@@ -117,30 +123,56 @@ public class InterfaceCadastroCliente extends JFrame {
 		lblNewLabel_4.setBounds(20, 121, 46, 14);
 		contentPane.add(lblNewLabel_4);
 		
-		textField_6 = new JTextField();
-		textField_6.setBounds(76, 118, 303, 20);
-		contentPane.add(textField_6);
-		textField_6.setColumns(10);
+		EmailLabel = new JTextField();
+		EmailLabel.setBounds(76, 118, 303, 20);
+		contentPane.add(EmailLabel);
+		EmailLabel.setColumns(10);
 		
 		JLabel lblNewLabel_5 = new JLabel("Usuario:");
 		lblNewLabel_5.setBounds(20, 170, 62, 14);
 		contentPane.add(lblNewLabel_5);
 		
-		textField_7 = new JTextField();
-		textField_7.setBounds(69, 167, 150, 20);
-		contentPane.add(textField_7);
-		textField_7.setColumns(10);
+		usuarioLabel = new JTextField();
+		usuarioLabel.setBounds(69, 167, 150, 20);
+		contentPane.add(usuarioLabel);
+		usuarioLabel.setColumns(10);
 		
 		JLabel lblNewLabel_6 = new JLabel("Senha:");
 		lblNewLabel_6.setBounds(20, 205, 62, 14);
 		contentPane.add(lblNewLabel_6);
 		
-		textField_8 = new JTextField();
-		textField_8.setBounds(69, 202, 150, 20);
-		contentPane.add(textField_8);
-		textField_8.setColumns(10);
+		SenhaLabel = new JTextField();
+		SenhaLabel.setBounds(69, 202, 150, 20);
+		contentPane.add(SenhaLabel);
+		SenhaLabel.setColumns(10);
 		
 		JButton btnNewButton = new JButton("Confirmar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// -- Salvar o cliente --
+				String cpf = CPFlabel.getText();
+				String usuario = usuarioLabel.getText();
+				//String senha = SenhaLabel.getText();
+				String email = EmailLabel.getText();
+				String endereco = EnderecoLabel.getText();
+				
+				// Data tem que implementar.
+				Data d = new Data(01,01,10);
+				
+				try(Cliente c = new Cliente(cpf,usuario,email,d,endereco,d,d)){
+					clientes.cadastrar(c); //Cadastro o cliente.
+					clientes.listar();
+				}
+				catch() {
+					System.out.println("Error: ");
+				}
+				
+				
+				
+				
+			}
+		});
+		
 		btnNewButton.setBounds(459, 229, 104, 23);
 		contentPane.add(btnNewButton);
 		
