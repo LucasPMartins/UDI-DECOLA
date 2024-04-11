57f61c2da68c24cf1ec2e4c2c3c4d5fe30037214
Breno Oliveira Cavalcante <breno.cavalcante@ufu.br>
2024-04-11 13:57:54 -0300

    banco e arquivo

--- PooEclipse/src/dao/Conexao.java
+++ PooEclipse/src/dao/Conexao.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Conexao {
 	
 	public Connection getConnection() throws SQLException{
-		Connection conexao = DriverManager.getConnection("jdbc:postgresql://localhost:5432/banco_cliente", "1", "1");
+		Connection conexao = DriverManager.getConnection("jdbc:postgresql://localhost:5432/banco_cliente", "postgres", "1");
 		return conexao;
 	}
 }
