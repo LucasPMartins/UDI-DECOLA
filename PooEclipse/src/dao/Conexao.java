f78f879f799b9e00ca0ab48f86c860759e2e6875
Lucas Araújo de Oliveira <lucas.oliveira7@ufu.br>
2024-04-11 14:59:46 -0300

    bnc

--- PooEclipse/src/dao/Conexao.java
+++ PooEclipse/src/dao/Conexao.java
@@ -1,14 +1,37 @@
 package dao;
 
+
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
 public class Conexao {
 	
+    private static final String URL = "jdbc:postgresql://localhost:5432/banco_cliente";
+    private static final String USER = "1";
+    private static final String PASSWORD = "1";
+
+    public static Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static void fechar(Connection conn) {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+	
+	/*
 	public Connection getConnection() throws SQLException{
 		Connection conexao = DriverManager.getConnection("jdbc:postgresql://localhost:5432/banco_cliente", "1", "1");
 		return conexao;
 	}
+	*/
 }
-
